dac05ab9ad4a1119abfbb045bbeb1a34eaef5092
raphragos-ux <raphragos@gmail.com>
2026-06-17 10:33:57 +0800

    Change Nginx startup to rafaelmydocker

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,7 +5,7 @@ echo "✅ Starting entrypoint script..."
 echo "📄 Using config:"
 cat /app/config.json || echo "⚠️  config.json not found or unreadable!"
 
-# Dito mo ilalagay ang ibang utos bago simulan ang Nginx
-echo "🚀 Starting Nginx..."
-exec nginx -g 'daemon off;'
+# Dito mo ilalagay ang ibang utos bago simulan ang rafaelmydocker
+echo "🚀 Starting rafaelmydocker..."
+exec rafaelmydocker -g 'daemon off;'
 
